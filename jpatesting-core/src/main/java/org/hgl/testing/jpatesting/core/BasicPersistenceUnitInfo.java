0d4eb4ca466e8e729cd16dff2c1348f15e50f215
jlhuerfanor <huerfano.jerson@gmail.com>
2020-06-13 12:33:43 -0500

    Project setup

---
+++ jpatesting-core/src/main/java/org/hgl/testing/jpatesting/core/BasicPersistenceUnitInfo.java
@@ -0,0 +1,56 @@
+package org.hgl.testing.jpatesting.core;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+import javax.persistence.SharedCacheMode;
+import javax.persistence.ValidationMode;
+import javax.persistence.spi.ClassTransformer;
+import javax.persistence.spi.PersistenceUnitInfo;
+import javax.persistence.spi.PersistenceUnitTransactionType;
+import javax.sql.DataSource;
+import java.net.URL;
+import java.util.List;
+import java.util.Properties;
+
+@Getter
+@NoArgsConstructor
+@SuperBuilder(toBuilder = false)
+public class BasicPersistenceUnitInfo implements PersistenceUnitInfo {
+    private String persistenceUnitName;
+    private String persistenceProviderClassName;
+    private PersistenceUnitTransactionType transactionType;
+    private DataSource jtaDataSource;
+    private DataSource nonJtaDataSource;
+    private List<String> mappingFileNames;
+    private List<URL> jarFileUrls;
+    private URL persistenceUnitRootUrl;
+    private List<String> managedClassNames;
+    private boolean excludeUnlistedClasses;
+    private SharedCacheMode sharedCacheMode;
+    private ValidationMode validationMode;
+    private Properties properties;
+    private String persistenceXMLSchemaVersion;
+    private List<ClassTransformer> transformers;
+
+    @Override
+    public boolean excludeUnlistedClasses() {
+        return this.isExcludeUnlistedClasses();
+    }
+
+    @Override
+    public ClassLoader getClassLoader() {
+        return Thread.currentThread().getContextClassLoader();
+    }
+
+    @Override
+    public void addTransformer(ClassTransformer classTransformer) {
+        this.transformers.add(classTransformer);
+    }
+
+    @Override
+    public ClassLoader getNewTempClassLoader() {
+        return null;
+    }
+}
